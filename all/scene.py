13925b74ed2a6ec701b7bbea31b31ee5671a664b
llomokhg <67365162+llomokhg@users.noreply.github.com>
2023-01-16 00:27:51 +0300

    Add files via upload

--- all/scene.py
+++ all/scene.py
@@ -1,7 +1,8 @@
 import prompt
 import pygame
-import all_else
+import areas
 import sys
+from random import choice
 
 
 def draw_region(cell_size, color, need_to_paint, window):
@@ -14,7 +15,7 @@ def draw_region(cell_size, color, need_to_paint, window):
 
 def redraw(window, cell_size, color_flag, selected):
     window.fill((0, 100, 240))
-    for country in all_else.countries:
+    for country in areas.countries:
         for area in country.areas:
             if color_flag == 'countries':
                 color = country.color
@@ -32,20 +33,17 @@ def redraw(window, cell_size, color_flag, selected):
                 draw_region(cell_size, (255, 73, 45), area.points, window)
             elif color_flag == 'country_select' and country in selected.country.neighbors:
                 draw_region(cell_size, (255, 181, 85), area.points, window)
-            elif color_flag == 'friend_select' and country == selected.country:
+            elif color_flag == 'relationship_select' and country == selected.country:
                 draw_region(cell_size, (13, 200, 15), area.points, window)
-            elif color_flag == 'friend_select' and country in selected.country.contracts.keys() and \
+            elif color_flag == 'relationship_select' and country in selected.country.pacts.keys() and \
+                    selected.country.pacts[country] != 0:
+                draw_region(cell_size, (120, 244, 167), area.points, window)
+            elif color_flag == 'relationship_select' and country in selected.country.contracts.keys() and \
                     selected.country.contracts[country] == 'union':
-                draw_region(cell_size, (73, 255, 45), area.points, window)
-            elif color_flag == 'enemy_select' and country == selected.country:
-                draw_region(cell_size, (13, 200, 15), area.points, window)
-            elif color_flag == 'enemy_select' and country in selected.country.contracts.keys() and \
+                draw_region(cell_size, (190, 129, 244), area.points, window)
+            elif color_flag == 'relationship_select' and country in selected.country.contracts.keys() and \
                     selected.country.contracts[country] == 'war':
                 draw_region(cell_size, (255, 73, 45), area.points, window)
-            elif color_flag == 'near_countries_select' and country == selected.country:
-                draw_region(cell_size, (255, 73, 45), area.points, window)
-            elif color_flag == 'near_countries_select' and country in selected.country.near_countries:
-                draw_region(cell_size, (255, 181, 85), area.points, window)
             else:
                 draw_region(cell_size, color, area.points, window)
     window.blit(pygame.font.SysFont('Comic Sans MS', 30).render(color_flag, False, (255, 0, 0)), (10, 750))
@@ -61,24 +59,69 @@ class Main_Scene:
         self.now_selected = app.now_selected
         self.screen = app.screen
 
+    def next_turn(self):
+        for country in areas.countries:
+            country.next_turn()
+
+        for country in areas.countries:
+            for area in country.areas:
+                if area.damage <= 0:
+                    for country1 in areas.countries:
+                        for general in country1.generals:
+                            if general.area == area and general.country != area.country:
+                                general.country.generals = \
+                                    general.country.generals[:general.country.generals.index(general)] + \
+                                    general.country.generals[general.country.generals.index(general) + 1:]
+                                general.area = None
+                                general.country = None
+                else:
+                    generals = []
+                    for country1 in areas.countries:
+                        for general in country1.generals:
+                            generals.append(general)
+                    area.change_country(choice(generals).country, self.board)
+                area.damage = 0
+
     def save(self):
         saved = []
-        for country in all_else.countries:
+        for country in areas.countries:
             country_arg = {}
             for country_key in country.__dict__.keys():
                 if country_key == 'areas':
-                    areas = []
+                    country_areas = []
                     for region in country.__dict__[country_key]:
                         area = {}
                         for region_key in region.__dict__.keys():
                             if region_key == 'country':
                                 area[region_key] = region.__dict__[region_key].number
+                            elif region_key == 'governor':
+                                if region.__dict__[region_key]:
+                                    area[region_key] = region.__dict__[region_key].characteristics
+                                else:
+                                    area[region_key] = None
+                            elif region_key == 'buildings':
+                                area[region_key] = []
+                                if region.__dict__[region_key]:
+                                    for building in region.__dict__[region_key]:
+                                        if building.name == 'University' or building.name == 'ArmyAcademy':
+                                            area[region_key].append({'name': building.name,
+                                                                     'data': building.data,
+                                                                     'year': building.year})
+                                        else:
+                                            area[region_key].append({'name': building.name,
+                                                                     'data': building.data})
+                            elif region_key == 'characteristics':
+                                area[region_key] = region.__dict__[region_key]
+                            elif region_key == 'probabilities':
+                                pass
+                            elif region_key == 'neighbors':
+                                pass
                             else:
                                 area[region_key] = region.__dict__[region_key]
-                        areas.append(area)
-                    country_arg[country_key] = areas
+                        country_areas.append(area)
+                    country_arg[country_key] = country_areas
                 elif country_key == 'neighbors':
-                    country_arg[country_key] = [neighbor.number for neighbor in country.__dict__[country_key]]
+                    pass
                 else:
                     country_arg[country_key] = country.__dict__[country_key]
             saved.append(country_arg)
@@ -88,21 +131,16 @@ class Main_Scene:
         pygame.display.flip()
         bad_place = []
 
-        try:
-            for country in all_else.countries:
-                for area in country.areas:
-                    area.set_neighbors(self.board.board)
-                country.set_neighbors()
-                country.set_near_countries(self.board.board)
-        except IndexError:
-            pass
+        for country in areas.countries:
+            for area in country.areas:
+                area.set_neighbors(self.board)
+            country.set_neighbors()
 
         running = True
         while running:
             for event in pygame.event.get():
 
                 if event.type == pygame.QUIT:
-
                     running = False
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -114,30 +152,29 @@ class Main_Scene:
                                 for x in range(window.width // self.cell_size):
                                     bad_place.append([window.pos[1] // self.cell_size + y,
                                                       window.pos[0] // self.cell_size + x])
+
                     if [self.board.get_cell(event.pos)[1], self.board.get_cell(event.pos)[0]] not in bad_place:
                         bad_place = []
                         if event.button == 1:
                             if board_pos != 0:
-                                self.now_selected = all_else.countries[board_pos[0]].areas[board_pos[1]]
+                                self.now_selected = areas.countries[board_pos[0]].areas[board_pos[1]]
 
                         if event.button == 3:
                             if self.layer == 'countries' or self.layer == 'country_select' or \
-                                    self.layer == 'near_countries_select' or \
-                                    self.layer == 'friend_select' or self.layer == 'enemy_select':
+                                    self.layer == 'relationship_select':
                                 if board_pos != 0:
-                                    huge_text = all_else.countries[board_pos[0]].name
-                                    text = ''.join(str([i.name for i in
-                                                        all_else.countries[board_pos[0]].areas])[1:-1]
-                                                   .split('\'')).split(', ')
+                                    huge_text = areas.countries[board_pos[0]].name
+                                    text = [f'{i}: {k}' for i, k in
+                                            areas.countries[board_pos[0]].characteristics.items()]
                                 else:
                                     huge_text = 'Water'
                                     text = 'It\'s water, just water'
 
                             elif self.layer == 'areas' or self.layer == 'area_select':
                                 if board_pos != 0:
-                                    huge_text = all_else.countries[board_pos[0]].name + ' ' + \
-                                                all_else.countries[board_pos[0]].areas[board_pos[1]].name
-                                    text = all_else.countries[board_pos[0]].areas[board_pos[1]].buildings
+                                    huge_text = areas.countries[board_pos[0]].name + ' ' + \
+                                                areas.countries[board_pos[0]].areas[board_pos[1]].name
+                                    text = areas.countries[board_pos[0]].areas[board_pos[1]].buildings
                                 else:
                                     huge_text = 'Water'
                                     text = 'It\'s water, just water'
@@ -165,15 +202,11 @@ class Main_Scene:
                     elif event.key == pygame.K_F4 or event.key == pygame.K_4:
                         self.layer = 'area_select'
                     elif event.key == pygame.K_F5 or event.key == pygame.K_5:
-                        self.layer = 'near_countries_select'
-                    elif event.key == pygame.K_F6 or event.key == pygame.K_6:
-                        self.layer = 'friend_select'
-                    elif event.key == pygame.K_F7 or event.key == pygame.K_7:
-                        self.layer = 'enemy_select'
-                    elif event.key == pygame.K_F8 or event.key == pygame.K_8:
+                        self.layer = 'relationship_select'
+                    elif event.key == pygame.K_F6 or event.key == pygame.K_7:
                         self.layer = 'ground'
                     elif event.key == pygame.K_r:
-                        for country in all_else.countries:
+                        for country in areas.countries:
                             country.next_turn()
                     elif event.key == pygame.K_c:
                         self.windows.clear()
